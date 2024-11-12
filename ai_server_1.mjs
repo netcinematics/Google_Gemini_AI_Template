d304431270769cad2ecca3d3c1d822d38725cee7
NetCinematics <alpinefalcon@protonmail.com>
2024-11-11 19:20:59 -0800

    init

---
+++ ai_server_1.mjs
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+console.log('Alive:')
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Write a story about a magic backpack.";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
